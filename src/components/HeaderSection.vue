4a8f7dfd638ce6ad919f77ea95a6d6e6c501fe97
Sagar Solanki <59771375+sagars0600@users.noreply.github.com>
2023-07-18 14:55:19 +0530

    UI chnages

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <el-button type="success">Success</el-button>
+</template>
+<script>
+
+export default {
+  name: 'HeaderSection',
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MenuBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <el-button type="success">Success</el-button>
+</template>
+<script>
+
+export default {
+  name: 'MenuBar',
+  
+}
+</script>
+
+<style scoped>
+
+</style>
